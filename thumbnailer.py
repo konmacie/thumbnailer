9fc1b7e370a6a32ca2204fb90e41deef9c0fa20b
Konrad Macieja <konrad.macieja@gmail.com>
2019-04-05 23:15:08 +0200

    added argparse

--- thumbnailer.py
+++ thumbnailer.py
@@ -2,6 +2,7 @@
 import glob
 from PIL import Image
 import os
+import argparse
 
 
 valid_ext = ['.jpg', '.jpeg', '.png']
@@ -25,7 +26,7 @@ def parse_img_path(img_path):
     return (img_path, file_name, file_ext, dirpath)
 
 
-def img_scan(path="./", recursive=False, extension=None):
+def img_scan(search_path="./", recursive=False, extension=None):
     '''
     Returns list of images in given path and, if recursive set to True,
     in its subdirectories.
@@ -33,9 +34,9 @@ def img_scan(path="./", recursive=False, extension=None):
 
     # prepare arguments for glob search
     if recursive:
-        path = os.path.join(path, '**/*{ext}')
+        search_path = os.path.join(search_path, '**/*{ext}')
     else:
-        path = os.path.join(path, '*{ext}')
+        search_path = os.path.join(search_path, '*{ext}')
 
     if extension:
         exts = [extension, ]
@@ -46,7 +47,7 @@ def img_scan(path="./", recursive=False, extension=None):
     images = []
     for ext in exts:
         images_tmp = glob.glob(
-            path.format(ext=ext),
+            search_path.format(ext=ext),
             recursive=recursive
         )
         # for img in images_tmp:
@@ -84,14 +85,53 @@ def create_thumbnail(img_path, prefix="tn_", postfix="", size=(300, 300)):
 
 
 if __name__ == "__main__":
-    images = img_scan(recursive=True)
+    parser = argparse.ArgumentParser(
+        description="Creates thumbnails from images found in source directory"
+    )
+    parser.add_argument('dir', nargs='?', default='./',
+                        help=('set to change source directory. '
+                              'Defaults to current directory.'))
+    parser.add_argument('-r', '--recursive', dest='recursive',
+                        help='process images in subdirectories',
+                        action='store_true')
+    parser.add_argument('--width', dest='width',
+                        type=int, metavar='W',
+                        help='maximal width of the thumbnail')
+    parser.add_argument('--height', dest='height',
+                        type=int, metavar='H',
+                        help='maximal height of the thumbnail')
+    parser.add_argument('-e', '--ext', dest='ext',
+                        type=str, metavar='EXT',
+                        help='process only images with given extension')
+    args = vars(parser.parse_args())
+
+    # TODO: add prefix and postfix args to parser
+
+    # prepare size variable for image processing
+    size = (args["width"] or 300, args["height"] or 300)
+
+    # prepare extension variable for file search
+    ext = args['ext']
+    if ext and not ext.startswith('.'):
+        ext = '.' + ext
+
+    # search for images
+    images = img_scan(
+        search_path=args['dir'],
+        recursive=args['recursive'],
+        extension=ext
+    )
+
+    # initiate stats
     total = len(images)
     print("Found {} images.".format(total))
     processed = 0
     ignored = 0
+
+    # create thumbnails
     for i in images:
         try:
-            create_thumbnail(i)
+            create_thumbnail(i, size=size)
         except ImageIsThumbnail as exc:
             print(exc)
             ignored += 1
